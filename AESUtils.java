f71e70f55943239977b5cd0273b72fe31524e5cd
ruwanswarna <ruwanswarna@gmail.com>
2024-08-31 22:59:09 +0530

    updated eceyption decryption class to match ChatClient1 and ChatClient2

--- AESUtils.java
+++ AESUtils.java
@@ -1,28 +1,33 @@
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 public class AESUtils {
-    private SecretKeySpec secretKey;
 
-    public AESUtils(byte[] key) {
-        this.secretKey = new SecretKeySpec(key, "AES");
+    private static final String AES = "AES";
+
+    // Method to generate a new AES key
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        keyGenerator.init(128); // Key size (128 bits is common)
+        return keyGenerator.generateKey();
     }
 
-    // Encrypt a message using AES
-    public String encrypt(String message) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES");
+    // Method to encrypt data using the AES key
+    public static String encrypt(String data, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(AES);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
-    // Decrypt a message using AES
-    public String decrypt(String encryptedMessage) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES");
+    // Method to decrypt data using the AES key
+    public static String decrypt(String encryptedData, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(AES);
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decodedBytes = Base64.getDecoder().decode(encryptedMessage);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedData);
         byte[] decryptedBytes = cipher.doFinal(decodedBytes);
         return new String(decryptedBytes);
     }
